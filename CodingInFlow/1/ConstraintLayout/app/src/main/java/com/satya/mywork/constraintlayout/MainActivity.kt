a1e41d709cf0b6a1b8d4f16444f71fe2c8e03a39
SATYAPRIYAARCH <satyapriya.arch@gmail.com>
2020-09-06 15:35:34 +0530

    add first

--- CodingInFlow/1/ConstraintLayout/app/src/main/java/com/satya/mywork/constraintlayout/MainActivity.kt
+++ CodingInFlow/1/ConstraintLayout/app/src/main/java/com/satya/mywork/constraintlayout/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.satya.mywork.constraintlayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.ActionBar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+
+            val intent = Intent(this,Dashboard::class.java)
+            startActivity(intent)
+        },3000)
     }
 }
